099df38e9f292094ccca9409b333ae929e5970be
Esteban González Matamoros <esteb.gonza29@gmail.com>
2020-06-12 16:56:46 -0600

    Tabla RatesTable

---
+++ Proy1_Diseno_InvestSys/Proy1_Diseno_InvestSys/Controller/RatesTable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proy1_Diseno_InvestSys.Controller
+{
+    class RatesTable
+    {
+        private int rows;
+        private int columns;
+
+        RatesTable(int rows, int columns)
+        {
+            this.rows = rows;
+            this.columns = columns;
+        }
+
+        public void addRow()
+        {
+            rows += 1;
+        }
+    }
+}
