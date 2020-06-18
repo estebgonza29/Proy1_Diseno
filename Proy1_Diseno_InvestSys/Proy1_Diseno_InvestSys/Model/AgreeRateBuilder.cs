69a44011e0678d96a3917f2fad74bc2808f76551
Esteban González Matamoros <esteb.gonza29@gmail.com>
2020-06-18 14:54:28 -0600

    Final de logica del Builder
    Se agregan los JSon de cada tipo de inversion con sus datos y dentro de los builders se pasan del json a la matriz del RatesTable

--- Proy1_Diseno_InvestSys/Proy1_Diseno_InvestSys/Model/AgreeRateBuilder.cs
+++ Proy1_Diseno_InvestSys/Proy1_Diseno_InvestSys/Model/AgreeRateBuilder.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace Proy1_Diseno_InvestSys.Model
 {
@@ -15,7 +17,16 @@ namespace Proy1_Diseno_InvestSys.Model
         }
 
         public void buildTable()
-        {
+        {            
+            JToken token = JObject.Parse(File.ReadAllText(@"JSonFiles/AgreedRate.json"));
+            Object[][] listaDatos = (token.SelectToken("Data")).ToObject<Object[][]>(); 
+            for(int i = 0; i < _ratesTable.rows; i++)
+            {
+                for(int j = 0; j < _ratesTable.columns; j++)
+                {
+                    _ratesTable.matrix[i][j] = (float)listaDatos[i][j];
+                }
+            }
             
         }
 
