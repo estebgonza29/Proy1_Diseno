f93922b68c2e1f74dbfc3ff0a5dc11aa0b32a2dc
josue0702 <josue990207@hotmail.com>
2020-06-18 20:22:27 -0600

    Preliminar completed controller
    Testing needed

--- Proy1_Diseno_InvestSys/Proy1_Diseno_InvestSys/Controller/CSVLogger.cs
+++ Proy1_Diseno_InvestSys/Proy1_Diseno_InvestSys/Controller/CSVLogger.cs
@@ -7,8 +7,22 @@ using System.Threading.Tasks;
 namespace Proy1_Diseno_InvestSys.Controller
 {
     class CSVLogger : Logger{
-        public void log(string data) { 
-            
+        public void log(string data) {
+            try
+            {
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter("Log.csv", true)) {
+                    string[] lines = data.Split(',');
+                    string line = "";
+                    for (int i = 0; i < lines.Length; i++) {
+                        if (i == lines.Length - 1) line += lines[i].Split(':')[1];
+                        else line += lines[i].Split(':')[1] + ',';
+                    }
+                    file.WriteLine(line);
+                }
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
--- Proy1_Diseno_InvestSys/Proy1_Diseno_InvestSys/Controller/XMLLogger.cs
+++ Proy1_Diseno_InvestSys/Proy1_Diseno_InvestSys/Controller/XMLLogger.cs
@@ -3,12 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+using System.Security.Cryptography;
 
 namespace Proy1_Diseno_InvestSys.Controller
 {
     class XMLLogger : Logger {
-        public void log(string data) { 
-            
+        public void log(string data) {
+            XmlDocument doc = new XmlDocument();
+
+            try
+            {
+                doc.Load("Log.xml");
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                doc.LoadXml(
+                    "<?xml version=\"1.0\"?> \n" +
+                    "<Investments> \n" +
+                    "</Investments> \n"
+                );
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
+
+            string[] nodes = data.Split(',');
+            string[] names = new string[nodes.Length];
+            string[] values = new string[nodes.Length];
+            for (int i = 0; i < nodes.Length; i++) {
+                string[] temp = nodes[i].Split(':');
+                names[i] = temp[0];
+                values[i] = temp[1];
+            }
+
+            XmlNode inv = doc.CreateElement("Investment");
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                XmlNode node = doc.CreateElement(names[i]);
+                node.InnerText = values[i];
+                inv.AppendChild(node);
+            }
+
+            doc.DocumentElement.AppendChild(inv);
+            try
+            {
+                doc.Save("Log.xml");
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
